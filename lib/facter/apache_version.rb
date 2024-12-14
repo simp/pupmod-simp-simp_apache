850bcc4fb343376371a58e5c83cff1018d4229e6
Steven Pritchard <steve@sicura.us>
2024-12-13 20:57:05 -0600

    Clean up for rubocop

--- lib/facter/apache_version.rb
+++ lib/facter/apache_version.rb
@@ -3,17 +3,17 @@
 #
 # Returns 'unknown' if the version cannot be determined.
 #
-Facter.add("apache_version") do
+Facter.add('apache_version') do
   apachectl = Facter::Core::Execution.which('apachectl')
   confine { apachectl }
 
   setcode do
     apache_version = 'unknown'
     begin
-      %x{#{apachectl} -v}.to_s.split("\n").first =~ /((\d\.?)+)/
-      apache_version = $1 if not $1.to_s.empty?
+      `#{apachectl} -v`.to_s.split("\n").first =~ %r{((\d\.?)+)}
+      apache_version = Regexp.last_match(1) unless Regexp.last_match(1).to_s.empty?
     rescue Errno::ENOENT
-      #No-op because we only care that the version is unknown if we can't execute a version check.
+      # No-op because we only care that the version is unknown if we can't execute a version check.
     end
     apache_version
   end
--- spec/classes/ssl_spec.rb
+++ spec/classes/ssl_spec.rb
@@ -12,40 +12,40 @@ describe 'simp_apache::ssl' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('simp_apache') }
           it { is_expected.to create_class('simp_apache::ssl') }
-          it { is_expected.to_not create_iptables__listen__tcp_stateful('allow_https') }
-          it { is_expected.to_not create_class('pki') }
-          it { is_expected.to_not create_pki__copy('simp_apache') }
-          it { is_expected.to_not contain_class('haveged') }
+          it { is_expected.not_to create_iptables__listen__tcp_stateful('allow_https') }
+          it { is_expected.not_to create_class('pki') }
+          it { is_expected.not_to create_pki__copy('simp_apache') }
+          it { is_expected.not_to contain_class('haveged') }
         end
 
         context 'firewall = true' do
-          let(:params){{ :firewall => true }}
+          let(:params) { { firewall: true } }
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('simp_apache') }
           it { is_expected.to create_class('simp_apache::ssl') }
           it { is_expected.to create_iptables__listen__tcp_stateful('allow_https') }
-          it { is_expected.to_not create_class('pki') }
-          it { is_expected.to_not create_pki__copy('simp_apache') }
+          it { is_expected.not_to create_class('pki') }
+          it { is_expected.not_to create_pki__copy('simp_apache') }
         end
 
         context 'pki = simp' do
-          let(:params){{ :pki => 'simp' }}
+          let(:params) { { pki: 'simp' } }
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('simp_apache') }
           it { is_expected.to create_class('simp_apache::ssl') }
-          it { is_expected.to_not create_iptables__listen__tcp_stateful('allow_https') }
+          it { is_expected.not_to create_iptables__listen__tcp_stateful('allow_https') }
           it { is_expected.to contain_class('pki') }
           it { is_expected.to create_pki__copy('simp_apache') }
           it { is_expected.to create_file('/etc/pki/simp_apps/simp_apache/x509') }
         end
 
         context 'with haveged = false' do
-          let(:params) {{:haveged => false}}
-          it { is_expected.to_not contain_class('haveged') }
-        end
+          let(:params) { { haveged: false } }
 
+          it { is_expected.not_to contain_class('haveged') }
+        end
       end
     end
   end
